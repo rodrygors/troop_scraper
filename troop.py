9d74bb9609c4880b450a7ed1ab6c06d16ce76d80
Rodrigo Ramos da Silva <a35479@alunos.ipb.pt>
2021-01-03 19:09:56 +0000

    Automated player name and id collection

--- troop.py
+++ troop.py
@@ -13,28 +13,33 @@ class Player:
     def __init__(self, id, name):
         self.id = id
         self.name = name
+    
+    def __str__(self): return ('Nome: ' +self.name+ '\nID: ' +self.id)
 
 #choose the source folder of the script and the destination folder of the excel file
 src = 'C:/Users/rodry/OneDrive/Documentos/Projetos Python/troopscraper/TW_scraper_git/troops.xls'
 dst = './data/troops_count.xls'
 
+#excel stuff
 workbook = xlwt.Workbook()
 sheet = workbook.add_sheet("Troop Counter", cell_overwrite_ok=False) 
 style = xlwt.easyxf('font: bold 1, color blue;')
 bold = xlwt.easyxf('font: bold 1')
 
-# ask for tribal wars credentials
+# ask for tribal wars credentials and other necessary info
 user = input ("Enter user :") 
 userpass = input("Enter password : ")  
+tribe_id = input("Enter your tribe ID : ")
 
 #get chromedriver and go to tribalwars
 driver = webdriver.Chrome(executable_path=r'./chromedriver.exe')
+extra_driver = webdriver.Chrome(executable_path=r'./chromedriver.exe')
+
 #hide browser
-driver.set_window_position(-10000,0)
-driver.get('https://enc4.tribalwars.net/game.php?village=1980&screen=ally&mode=members_troops')
+#driver.set_window_position(-10000,0)
 
-element = driver.find_element_by_tag_name('h2')
-
+#find login elements
+driver.get('https://enc4.tribalwars.net')
 username = driver.find_element_by_id("user")
 password = driver.find_element_by_id("password")
 
@@ -51,15 +56,35 @@ print(actualtitle)
 time.sleep(2)
 driver.find_element_by_xpath('//a[2]/span').click()
 
-#playersID array
-arr = [848937173 , 848941819, 848941982, 848945478, 848932450,848942150,848941598,848941934,848941896,848943143,10081546,11586147]
-arr_nomes = ['caracortada', 'Claudiac8', 'F.T.Freitas', 'Forcedarmy', 'Guts', 'Jarbas', 'julzz', 'LaTila', 'Lord Shadow', 'Meiguinho', 'rodrygors', 'wendig0xd']
+#Get tribe member's names and ids
+driver.get('https://enc4.tribalwars.net/game.php?village=1587&screen=ally&mode=members')
 arr_players = []
-for i in range(len(arr)):
-    p = Player(arr[i], arr_nomes[i])
+n_membros = int(driver.find_element_by_xpath('//form[@id=\'form_rights\']/table/tbody/tr[last()-1]/td[2]').text)
+print(n_membros)
+for i in range(n_membros):
+    member_name = driver.find_element_by_xpath('//form[@id=\'form_rights\']/table/tbody/tr[%s]/td/a' % (i+2)).text
+    print(member_name)
+
+    member_link = str(driver.find_element_by_xpath('//a[contains(text(),\'%s\')]' % member_name).get_attribute('href'))
+    
+    member_id = member_link.split('id=')
+    
+    try:
+        p = Player(member_id[1], member_name)
+    except:
+        extra_driver.get('https://www.twstats.com/enc4/index.php?page=tribe&mode=members&id=%s'% tribe_id)
+        member_link = str(extra_driver.find_element_by_xpath('//a[contains(text(),\'%s\')]' % member_name).get_attribute('href'))
+        member_id = member_link.split('id=')
+        print(member_id)
+        p = Player(member_id[1], member_name)
+        extra_driver.quit()
+    print(p)
     arr_players.append(p)
+
 #types of units
 troops_names = ['lanças','espadas','vikings','espias','cl','cp','arietes','catas','nobres','comandos','a chegar']
+
+#loops to read and print every village's units
 r = 0
 c = 0
 x = 0
@@ -70,7 +95,7 @@ r = 7
 for i in range(len(arr_players)):
     troops=[0] * 11
     c = 2
-    driver.get('https://enc4.tribalwars.net/game.php?screen=ally&mode=members_troops&player_id=%s&village=3680' % (arr_players[i].id))
+    driver.get('https://enc4.tribalwars.net/game.php?screen=ally&mode=members_troops&player_id=%s' % (arr_players[i].id))
     troops_table = driver.find_elements_by_css_selector('.w100 td')
     info = sheet.write(r-1, 0, arr_players[i].name, bold)
     x = r
